893225770a75ce5a0926f4eb7815d5ceaad3f8c8
Taehwan Kim <taehwankim@dhcp-10-250-216-178.harvard.edu>
2019-04-24 23:40:41 -0400

    move into the shiny app after deleting from outside

---
+++ chicago_crime/app.R
@@ -0,0 +1,64 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+#source("data/date_graph.R")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Reported Crime in Chicago Over Time"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("years",
+                     "Year Range",
+                     min = 2008,
+                     max = 2018,
+                     value = c(2008,2018))
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("yearGraph")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$yearGraph <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      year_range <- chicago %>% 
+        filter(year >= input$years[1]) %>% 
+        filter(year <= input$years[2]) %>% 
+        group_by(year) %>%
+        summarize(cases = n()) %>%
+        ungroup()
+        
+      # draw the histogram with the specified number of bins
+      year_range %>% 
+        ggplot(aes(x = year, y = cases)) +
+          geom_line() +
+          theme_calc() +
+          labs(title = "Number of Reported Cases",
+               subtitle = "In Chicago, IL",
+               caption = "Source: data.cityofchicago.org") +
+          xlab("Year") +
+          ylab("Number of Reported Cases") +
+          scale_y_continuous(labels = comma)
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
